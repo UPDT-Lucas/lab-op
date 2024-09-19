96b3c22b5ec142e580bdc2cbb4eb14a369f04384
UPDT-Lucas <lucas.oviedocastro07@gmail.com>
2024-09-18 23:38:41 -0600

    node-service

--- update-deployment.sh
+++ update-deployment.sh
@@ -1,19 +1,21 @@
-REPO_DIR=""
-SERVICE_NAME=""
+#!/bin/bash
+REPO_DIR="/home/alexander/lab"
+SERVICE_NAME="update-deployment"
 
 cd "$REPO_DIR" || exit
 
 git fetch origin
 
 LOCAL_COMMIT=$(git rev-parse HEAD)
-REMOTE_COMMIT=$(git rev-parse origin/Deployment)
+REMOTE_COMMIT=$(git rev-parse origin/deployment)
 
-if ["$LOCAL_COMMIT" != "$REMOTE_COMMIT"]; then
+if [ "$LOCAL_COMMIT" != "$REMOTE_COMMIT" ]; then
    echo "Actualizando el repo..."
    
-   git pull origin Deployment
+   git pull origin deployment
    
-   systemctl reload "$SERVICE_NAME"
+   sudo systemctl daemon-reload
+   sudo systemctl restart "$SERVICE_NAME"
 else
    echo "No hay cambios en la rama"
 fi
